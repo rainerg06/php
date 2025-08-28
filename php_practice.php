cfbf4f1d95c809c659e64f6aa15fa36eb8b4e281
rainerg06 <163095202+rainerg06@users.noreply.github.com>
2025-08-28 20:15:56 +0800

    Add files via upload

---
+++ php_practice.php
@@ -0,0 +1,154 @@
+<!-- Hello!
+<h1>Hello!</h1>
+<style>
+    h1 {
+        color: yellow;
+    }
+</style>
+<script>
+    alert("Hello!")
+</script> -->
+<?php 
+   /* echo "<b>Hello World!</b>";
+    echo "<b>Hello World!</b>";
+    echo "<b>Hello World!</b>";
+    echo "<b>Hello World!</b>";
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>Hello this is Me!</h1> -->
+    <?php 
+        // eChO "Hello";
+        
+        $color = "Red";
+        $Color = "Blue";
+        // echo $Color;
+        // echo $color;
+
+        $_ = 5;
+
+        // echo $_;
+
+        // var_dump($color);
+        // var_dump($_);
+        // var_dump(3.1);
+        // var_dump(false);
+        // var_dump([1,2]);
+
+        // VARIABLE
+        // $x = 5; //global
+
+        // function test() {
+        //   //  $x = 10; //local
+        //     static $x = 10;
+        //     echo $x;
+        //     $x++;
+        //     echo "<br>";
+        // }
+
+        // test();
+        // test();
+        // test();
+        // test();
+        // test();
+        // test();
+        // echo $x;
+
+        // echo & print
+
+        // echo ("Hello"), ("Hello"), ("Hello");
+        // echo  "World!", "World!";
+        // print ("Hello");
+        // print "World!";
+
+
+        // $x = print("");
+        // echo $x;
+
+
+        // $a = 'Meruem';
+        // $b = 100;
+        // $c = 100.00;
+        // $d = true;
+        // $e = [1,2,3];
+        // $f = new stdClass();
+        // $g = null;
+        // $h = fopen("php_practice.php", "r");
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+        // // echo strlen($a);
+       
+        // $x = "Juan Cruz";
+        // echo str_replace("Cruz", "Lance", $x);
+
+        // echo trim("Hello World!                         ");
+
+        // var_dump(explode("u", $x));
+
+       // echo "Hello World!";
+
+        // $x = "Hello";
+        // $y = "Meruem";
+
+        // echo $x . " " . $y, "<br>";
+        // echo "$x  $y","<br>";
+        // echo "Meruem \"Hari ng mga Langgam\" Estrella";
+
+        // echo is_int(5),"<br>";
+
+        // echo PHP_FLOAT_MIN,"<br>";
+        // echo PHP_FLOAT_MAX,"<br>";
+
+        // echo is_float(5.0);
+
+        // echo is_numeric("25");
+
+        $a = (int) '5';
+        $b = (int) 100;
+        $c = (int) 100.00;
+        $d = (int) true;
+        $g = (int) null;
+        $h = (int) fopen("php_practice.php", "r");
+
+
+        var_dump($a);
+        echo "<br>";
+        var_dump($b);
+        echo "<br>";
+        var_dump($c);
+        echo "<br>";
+        var_dump($d);
+        echo "<br>";
+        var_dump($g);
+        echo "<br>";
+        var_dump($h);
+        echo "<br>";
+
+    ?>
+</body>
+</html>
+
