e1997ae5785d9ed6f851be2cd140667720e2540c
rainerg06 <163095202+rainerg06@users.noreply.github.com>
2025-09-11 20:02:13 +0800

    Add files via upload

--- php_practice.php
+++ php_practice.php
@@ -28,12 +28,12 @@
     <?php 
         // eChO "Hello";
         
-        $color = "Red";
-        $Color = "Blue";
-        // echo $Color;
-        // echo $color;
+        // $color = "Red";
+        // $Color = "Blue";
+        // // echo $Color;
+        // // echo $color;
 
-        $_ = 5;
+        // $_ = 5;
 
         // echo $_;
 
@@ -127,28 +127,236 @@
 
         // echo is_numeric("25");
 
-        $a = (int) '5';
-        $b = (int) 100;
-        $c = (int) 100.00;
-        $d = (int) true;
-        $g = (int) null;
-        $h = (int) fopen("php_practice.php", "r");
+        // $a =  (object) '99 fist buddha Meruem';
+        // $b =  (object) 99;
+        // $c =  (object) 99.99;
+        // $d =  (object) true;
+        // $g =  (object) null;
+        // $h =  (object) fopen("php_practice.php", "r");
+
+        // unset($a);
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+        // $x = pi();
+        // echo $x;
+
+        // echo abs(-5);
+
+        // echo sqrt(4);
+
+        // echo round(5.4);
+
+        // echo rand(1,99);
+
+        // $x = 5;
+        // define("GREETING", "HELLO");
+        // // echo GREETING;
+
+        // function test(){
+        //     echo GREETING;
+        // }
+
+        // test();
+        // $x = 5; 
+
+        // $x = $x + 10;
+        // $x += 10;
+
+        // $x %= 5;
+        // echo $x;
+
+        // var_dump(5 == (int) "a5jdshdas");
+        // var_dump(5 === "5");
+        // var_dump(5 <> 7);
+        // var_dump(5 !== 5);
+
+        // echo 5 <=> 2; 
+        
+        // pre-increment/decrement
+        // $x = 5;
+        // echo ++$x;
+
+        // //post-increment/decrement
+        // $y = 5;
+        // echo $y++;
+        // echo $y;
+
+        //logical operator
+
+        // and &&
+        // var_dump(true && true); //true
+        // var_dump(5=="5" && 5==5); //true
+
+        // // OR || 
+        // var_dump(true or false); //true
+
+        //xor
+        // var_dump(true xor true); //false
+
+        // ! reverse
+        // var_dump(!false); // reverse value
+
+        // .
+        // $x = "Hello";
+
+        // .= append
+        // $x .= "World!";
+        // echo $x;
+
+        // array
+
+        //union
+
+        // $x = [1,2,3];
+        // $y = [3=>4,5,6,7];
+
+        // $z = $x + $y;
+
+        // // print_r($x);
+        // // print_r($y);
+        // print_r($z);
+
+        // //equal to
+        // $x = [1,2,3];
+        // $y = [2=>3,1=>2,0=>1];
+        // echo $x == $y; //true
+
+        // identical
+        // $x = [1,2,3];
+        // $y = [2=>3,1=>2,0=>1];
+        // echo $x === $y; //false
+
+        //decision making statements
+
+        // if statement
+
+        // if(4 == 5){
+        //     echo "same";
+        // }elseif(6>5){
+        //     echo "6 greater is greater than 5";
+        // }
+        // else{
+        //     echo "no";
+        // }
+
+        // $gender = null;
+        // // if($gender == "female"){echo "woman";}else{echo "mens";}
+       
+        // // // condiation ? if true : if false;
+        // // echo $gender == "female" ? "woman" : "mens";
+        // // $x = $gender == "female" ? "woman" : "mens";
+        // // echo $x;
+
+        // $x = $gender ?? null;
+
+        // echo $x;
+
+        // switch
+
+        // $color = "R";
+
+        // switch($color){
+        //     case "R":
+        //         echo "Red";
+        //         break;
+        //     case "G":
+        //         echo "Green";
+        //         break;
+        //     case "B":
+        //         echo "Blue";
+        //         break;
+        //     default:
+        //         echo "Invalid";
+        // }
 
 
-        var_dump($a);
-        echo "<br>";
-        var_dump($b);
-        echo "<br>";
-        var_dump($c);
-        echo "<br>";
-        var_dump($d);
-        echo "<br>";
-        var_dump($g);
-        echo "<br>";
-        var_dump($h);
-        echo "<br>";
+        // true amd true
+        // if (4 == "5" && 4 == 4) {}
+           
+
+        // // atlest 1 true
+        // if (4 == "4" || 4 == 4) {}
+
+        // // only 1 true cannot be both
+        // if (4 == "4" XOR 4 == 4) {}
+
+        // // not
+        // if (!4 == "4") {}
+
+        //php looping statement
+
+        // while
+        // $x = 0;
+        // while ($x < 5){
+        //     echo "Hello";
+        //     continue;
+        //     $x++;
+        // }
+
+        // //do while
+        // do {
+        //     echo $x;
+        //     $x++;
+        // }while($x < 5)
+
+        // for
+        // for($x = 0; $x<5; $x++){
+        //     echo $x;
+        // }
+
+        //foreach
+        // $x = array(1,2,3,4);
+
+        // foreach($x as $key => $value){
+        //     echo $key. "->"  .$value,"<br>";
+        // }
+
+      
+
+
+        
+
+
+
+
+
+
+
 
     ?>
+
+      <ol>
+            <!-- <?php 
+                for($x = 1; $x <= 10; $x++){ 
+                    echo "<li>Item $x</li>";
+                }   
+            ?> -->
+
+           
+            <?php
+            $x = array("Maria","Theresa", "Juana");
+
+            foreach($x as $value){
+                echo "<li>$value</li>";
+            }
+
+            ?>
+            
+           
+
+           
+        </ol>
 </body>
 </html>
-
